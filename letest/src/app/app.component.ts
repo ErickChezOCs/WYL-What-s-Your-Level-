1a4b67f9afc892941c300b41a2e2bea0a6af496b
ErickChezOCs <nounga72@yahoo.fr>
2023-04-11 20:05:56 +0200

    test2

--- letest/src/app/app.component.ts
+++ letest/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Testez votre niveau de compétences ';
+  title = 'Testez votre niveau de compétences en ';
 }
---
+++ letest/src/app/logiciels/logiciels.component.ts
@@ -0,0 +1,16 @@
+import { Component,  OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logiciels',
+  templateUrl: './logiciels.component.html',
+  styleUrls: ['./logiciels.component.css']
+})
+export class LogicielsComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+  logiciel = "Ms-Excel"
+}
