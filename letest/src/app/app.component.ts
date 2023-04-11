d021f10dd981fb25ef7c7560144f729c29d38ec1
ErickChezOCs <nounga72@yahoo.fr>
2023-04-11 19:34:54 +0200

    test_1

---
+++ letest/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Testez votre niveau de compétences ';
+}
