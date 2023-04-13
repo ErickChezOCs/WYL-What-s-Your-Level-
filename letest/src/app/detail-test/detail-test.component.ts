fea04ec19c5523fd180534d82193a9c94fa3552e
ErickChezOCs <nounga72@yahoo.fr>
2023-04-13 22:22:38 +0200

    test7

--- letest/src/app/detail-test/detail-test.component.ts
+++ letest/src/app/detail-test/detail-test.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Logiciel } from '../logiciel';
 
 @Component({
   selector: 'app-detail-test',
   templateUrl: './detail-test.component.html',
   styleUrls: ['./detail-test.component.css']
 })
+ @Input() logiciel?: Logiciel; 
+
 export class DetailTestComponent {
+  
 
 }
+ 
