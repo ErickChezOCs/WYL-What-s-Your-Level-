6f72d4a5765774973165405f3a8eb41e53162659
ErickChezOCs <nounga72@yahoo.fr>
2023-04-14 19:16:34 +0200

    testy

--- letest/src/app/detail-test/detail-test.component.ts
+++ letest/src/app/detail-test/detail-test.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Logiciel } from '../logiciel';
 
+
 @Component({
   selector: 'app-detail-test',
   templateUrl: './detail-test.component.html',
   styleUrls: ['./detail-test.component.css']
 })
- @Input() logiciel?: Logiciel; 
+ @Input()logiciel?: Logiciel; 
 
 export class DetailTestComponent {
   
